8e8c99219a55e465275a8342515f2303bdb64548
tong <tongshanre@163.com>
2018-02-22 18:34:26 +0800

    ubuntu

--- gpio_m.py
+++ gpio_m.py
@@ -3,9 +3,12 @@ import RPi.GPIO as GPIO
 class GPioUtil():
     __ports = [2,3,4,5]
     def __init__(self):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.__ports, GPIO.OUT)
+        try:
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(self.__ports, GPIO.OUT)
+        except BaseException     as e :
+            print e
     def change(self,id,status):
         id = int(id)
         if 2 <= id and 5 >= id :
@@ -13,6 +16,3 @@ class GPioUtil():
             return True;
         else:
             return False;
-
-
-
